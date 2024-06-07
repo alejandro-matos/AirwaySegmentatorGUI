3cf54e4adad2cfd96ebf433fcfb690aada693e34
alejandro-matos <84166343+alejandro-matos@users.noreply.github.com>
2024-06-07 00:59:30 -0600

    Update DtoN_GUI.py
    Added features such as renaming, name mapping, anonymization, anonymized name or pt name in name metadata, etc. Almost final version. Only need to edit instructions and minor details

--- nnUNetv2GUI/DtoN_GUI.py
+++ nnUNetv2GUI/DtoN_GUI.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import sys
 import styles
 import pydicom
+import shutil
+
 
 class AnonDtoNGUI(tk.Frame):
     def __init__(self, parent, home_callback):
@@ -15,10 +17,13 @@ class AnonDtoNGUI(tk.Frame):
         self.configure(bg=styles.BG_COLOR)
 
         self.input_path = tk.StringVar()
+        self.output_path = tk.StringVar()  # Added output path variable
         self.starting_number = tk.IntVar(value=1)  # Added starting number variable
         self.anonymize = tk.BooleanVar()
         self.convert_to_nifti = tk.BooleanVar()
         self.prepare_for_segmentation = tk.BooleanVar()
+        self.rename_files = tk.BooleanVar()  # Added rename files variable
+        self.data_nickname = tk.StringVar(value='Airways')
 
         self.create_widgets()
 
@@ -34,34 +39,32 @@ class AnonDtoNGUI(tk.Frame):
             self.input_path.set(selected_path)
             print(f"Input path set to: {selected_path}")
 
+    def browse_output_path(self):
+        selected_path = filedialog.askdirectory()
+        if selected_path:
+            self.output_path.set(selected_path)
+            print(f"Output path set to: {selected_path}")
+
     def open_folder(self):
         input_path_str = self.input_path.get()
-        if not input_path_str or not os.path.isdir(input_path_str):
-            messagebox.showwarning("Input Error", "Please select a valid directory.")
-        else:
-            if sys.platform == "win32":
-                os.startfile(input_path_str)
-            elif sys.platform == "darwin":
-                os.system(f"open {input_path_str}")
+        parent_directory = os.path.dirname(input_path_str)
+        processed_dir = os.path.join(parent_directory, f"{os.path.basename(input_path_str)}_Processed_Images")
+        if processed_dir:
+            if not os.path.isdir(processed_dir):
+                messagebox.showwarning("Directory Error", f"The directory {processed_dir} does not exist. Please ensure that the correct folder is selected and/or that it has been processed.")
             else:
-                os.system(f"xdg-open {input_path_str}")
+                print(f"Opening folder: {processed_dir}")
+                if sys.platform == "win32":
+                    os.startfile(processed_dir)
+                elif sys.platform == "darwin":
+                    os.system(f"open {processed_dir}")
+                else:
+                    os.system(f"xdg-open {processed_dir}")
 
-    def anonymize_dicom_folder(self, input_folder, output_folder, patient_index):
-        os.makedirs(output_folder, exist_ok=True)
-        start_number = self.starting_number.get()
-        for root, _, files in os.walk(input_folder):
-            for i, file_name in enumerate(files):
-                if file_name.lower().endswith(".dcm") or "DCM" in file_name:
-                    input_file_path = os.path.join(root, file_name)
-                    # Rename the file using the patient index and starting number
-                    anonymized_file_name = f"Anon_{patient_index}_{start_number + i}.dcm"
-                    output_file_path = os.path.join(output_folder, anonymized_file_name)
-                    self.anonymize_dicom(input_file_path, output_file_path)
-
-    def anonymize_dicom(self, input_file, output_file):
-        dataset = pydicom.dcmread(input_file)
+    def anonymize_dicom(self, input_file, output_file, anonymized_file_name, patient_name):
+        dataset = pydicom.dcmread(input_file, force=True)
         tags_to_anonymize = [
-            ("PatientName", "ANONYMIZED"),
+            ("PatientName", patient_name),
             ("PatientID", "ANONYMIZED"),
             ("PatientBirthDate", "N/A"),
             ("PatientSex", "N/A"),
@@ -73,71 +76,116 @@ class AnonDtoNGUI(tk.Frame):
 
     def convert_files(self):
         input_path_str = self.input_path.get()
-        output_path_str = self.input_path.get()
         anonymize = self.anonymize.get()
         convert_to_nifti = self.convert_to_nifti.get()
         prepare_for_segmentation = self.prepare_for_segmentation.get()
-
-        print(f"Input path: {input_path_str}")
-        print(f"Output path: {output_path_str}")
-        print(f"Anonymize: {anonymize}")
-        print(f"Convert to NIfTI: {convert_to_nifti}")
-        print(f"Prepare for segmentation: {prepare_for_segmentation}")
+        rename_files = self.rename_files.get()
+        data_nick = self.data_nickname.get()
 
         if not input_path_str:
             messagebox.showwarning("Input Error", "Please select the input directory.")
             return
 
-        # Create a base anonymized directory at the same level as input_path_str
-        base_anonymized_path = os.path.join(os.path.dirname(input_path_str), "Anonymized")
-        os.makedirs(base_anonymized_path, exist_ok=True)
+        # Get the parent directory of the input folder
+        parent_directory = os.path.dirname(input_path_str)
+        output_dir = os.path.join(parent_directory, f"{os.path.basename(input_path_str)}_Processed_Images")
+        self.output_path.set(output_dir)  # Set the output directory
+
+        central_nifti_folder = os.path.join(output_dir, "NIfTI")
+        os.makedirs(central_nifti_folder, exist_ok=True)
+
+        # File to store the mapping of original and anonymized folder names
+        if rename_files:
+            mapping_file_path = os.path.join(output_dir, "folder_mapping.txt")
+            with open(mapping_file_path, "w") as mapping_file:
+                mapping_file.write("Original Folder\tAnonymized Folder\n")
 
         patient_folders = [f for f in os.listdir(input_path_str) if os.path.isdir(os.path.join(input_path_str, f))]
-
-        for patient_index, patient_folder in enumerate(patient_folders):
+        start_number = self.starting_number.get()
+        print(f"This is the output of patient_folders: {patient_folders}")
+        for patient_index, patient_folder in enumerate(patient_folders, start=start_number):
+            print(f"Processing patient folder: {patient_folder} with index {patient_index}")
             patient_folder_path = os.path.join(input_path_str, patient_folder)
-            anonymized_folder_name = f"Anon_{patient_index}"
-            anonymized_folder_path = os.path.join(base_anonymized_path, anonymized_folder_name)
+
+            if rename_files:
+                anonymized_folder_name = f"{data_nick}_{patient_index}"  # Custom naming
+                # Log folder names to the mapping file
+                with open(mapping_file_path, "a") as mapping_file:
+                    mapping_file.write(f"{patient_folder}\t{anonymized_folder_name}\n")
+                patient_name = anonymized_folder_name
+            else:
+                anonymized_folder_name = patient_folder
+                patient_name = patient_folder
 
             if anonymize:
-                self.anonymize_dicom_folder(patient_folder_path, anonymized_folder_path, patient_index)
+                # Create base directories inside the output_dir
+                base_anonymized_path = os.path.join(output_dir, "Processed CBCTs")
+                os.makedirs(base_anonymized_path, exist_ok=True)
+                anonymized_folder_path = os.path.join(base_anonymized_path, anonymized_folder_name)
+                self.anonymize_dicom_folder(patient_folder_path, anonymized_folder_path, patient_index, patient_name)
             else:
                 anonymized_folder_path = patient_folder_path
 
             if convert_to_nifti:
-                output_patient_folder = os.path.join(anonymized_folder_path, "NIfTI")
-                os.makedirs(output_patient_folder, exist_ok=True)
                 try:
-                    dicom2nifti.convert_directory(anonymized_folder_path, output_patient_folder, compression=True)
+                    dicom2nifti.convert_directory(anonymized_folder_path, central_nifti_folder, compression=True)
                 except dicom2nifti.exceptions.ConversionError as e:
                     print(f"Error converting {anonymized_folder_path}: {e}")
                 except Exception as e:
                     print(f"Unexpected error: {e}")
 
-                for nifti_file in os.listdir(output_patient_folder):
-                    if nifti_file.endswith('.nii') or nifti_file.endswith('.nii.gz'):
-                        base_name, ext = os.path.splitext(nifti_file)
-                        if ext == ".gz":
-                            base_name, ext2 = os.path.splitext(base_name)
-                            new_name = f"Anon_{patient_index}_{base_name}{ext2}{ext}"
-                        else:
-                            new_name = f"Anon_{patient_index}_{base_name}{ext}"
-                        os.rename(os.path.join(output_patient_folder, nifti_file), os.path.join(output_patient_folder, new_name))
+                nifti_files = [f for f in os.listdir(central_nifti_folder) if f.endswith('.nii') or f.endswith('.nii.gz')]
+                for nifti_file in nifti_files:
+                    base_name, ext = os.path.splitext(nifti_file)
+                    if prepare_for_segmentation:
+                        suffix = "_0000"
+                    else:
+                        suffix = ""
 
-                if prepare_for_segmentation:
-                    suffix = "_0000"
-                    for nifti_file in os.listdir(output_patient_folder):
-                        if nifti_file.endswith('.nii') or nifti_file.endswith('.nii.gz'):
-                            base_name, ext = os.path.splitext(nifti_file)
-                            if ext == ".gz":
-                                base_name, ext2 = os.path.splitext(base_name)
-                                new_name = f"{base_name}{suffix}{ext2}{ext}"
-                            else:
-                                new_name = f"{base_name}{suffix}{ext}"
-                            os.rename(os.path.join(output_patient_folder, nifti_file), os.path.join(output_patient_folder, new_name))
+                    if ext == ".gz":
+                        base_name, ext2 = os.path.splitext(base_name)
+                        if rename_files:
+                            new_name = f"{data_nick}_{patient_index}{suffix}{ext2}{ext}"
+                        else:
+                            new_name = f"{patient_folder}{suffix}{ext2}{ext}"
+                    else:
+                        if rename_files:
+                            new_name = f"{data_nick}_{patient_index}{suffix}{ext}"
+                        else:
+                            new_name = f"{patient_folder}{suffix}{ext}"
+
+                    new_path = os.path.join(central_nifti_folder, new_name)
+                    if not os.path.exists(new_path):
+                        print(f"Renaming NIfTI file {nifti_file} to {new_name}")
+                        os.rename(os.path.join(central_nifti_folder, nifti_file), new_path)
 
         messagebox.showinfo("Process Complete", "The selected operations have been completed.")
 
+    def anonymize_dicom_folder(self, input_folder, output_folder, patient_index, patient_name):
+        os.makedirs(output_folder, exist_ok=True)
+        start_number = self.starting_number.get()
+        data_nick = self.data_nickname.get()
+        print(f"Anonymizing DICOM folder: {input_folder} -> {output_folder}")
+        for root, _, files in os.walk(input_folder):
+            for i, file_name in enumerate(files):
+                if file_name.lower().endswith(".dcm") or "DCM" in file_name:
+                    input_file_path = os.path.join(root, file_name)
+                    if self.rename_files.get():
+                        # Rename the file using the patient index and starting number
+                        anonymized_file_name = f"{data_nick}_{patient_index}_{1 + i}.dcm"
+                    else:
+                        anonymized_file_name = file_name
+                    output_file_path = os.path.join(output_folder, anonymized_file_name)
+                    self.anonymize_dicom(input_file_path, output_file_path, anonymized_file_name, patient_name)
+
+    def toggle_rename_fields(self):
+        if self.rename_files.get():
+            self.nick_label_entry.config(state=tk.NORMAL)
+            self.start_number_entry.config(state=tk.NORMAL)
+        else:
+            self.nick_label_entry.config(state=tk.DISABLED)
+            self.start_number_entry.config(state=tk.DISABLED)
+            
     def create_widgets(self):
         home_button = tk.Button(self, text="Home", command=self.home_callback, bg='#FF9800', fg='white',
                                 font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE), padx=styles.BUTTON_PADDING, pady=styles.BUTTON_PADDING)
@@ -150,16 +198,17 @@ class AnonDtoNGUI(tk.Frame):
                    
         Instructions:
         - Use the toggle buttons to select tasks:
-            - Anonymize: Removes personal information and numbers anonymized files starting 
-                from the specified number in the "Starting number" line. A .txt file containing the 
-                naming key relating the original and anonymized files is also created in this step.
-            - Convert to NIfTI: Converts DICOM files to NIfTI (.nii.gz) format.
-            - Add Suffix for Segmentation: Adds '_0000' to the file names for segmentation use.
+                - Anonymize: Removes personal information and numbers anonymized files starting 
+                    from the specified number in the "Starting number" line. A .txt file containing the 
+                    naming key relating the original and anonymized files is also created in this step.
+                - Convert to NIfTI: Converts DICOM files to NIfTI (.nii.gz) format.
+                - Add Suffix for Segmentation: Adds '_0000' to the file names for segmentation use.
         - Click 'Browse' to select the folder containing the files.
-        - Input the starting number for anonymized files.
+        - Input the starting number for numbering the anonymized files.
+        - Make sure that the folder contains only the DICOM files to be anonymized and/or converted.
         - Click 'Start' to begin processing the selected tasks.
         - Once done, click 'Open Folder' to access the processed images and renaming key in the newly 
-          created folder called "Processed_Images".""")
+          created folder called "{Folder Name}_Processed_Images".""")
         text_widget.grid(row=1, column=0, columnspan=3, padx=20, pady=(10, 20))
         text_widget.tag_configure("bold", font=(styles.FONT_FAMILY, 18, 'bold'))
         text_widget.tag_configure("center", justify='center')
@@ -190,19 +239,30 @@ class AnonDtoNGUI(tk.Frame):
         segmentation_toggle = tk.Checkbutton(self, variable=self.prepare_for_segmentation, bg=styles.BG_COLOR)
         segmentation_toggle.grid(row=roww+2, column=2, sticky="EW", padx=(20, 0))
 
+        # Add option asking whether to rename files or not
+        rename_label = tk.Label(self, text="Rename files?", **label_style)
+        rename_label.grid(row=roww+3, column=0, **label_grid)
+        rename_toggle = tk.Checkbutton(self, variable=self.rename_files, bg=styles.BG_COLOR, command=self.toggle_rename_fields)
+        rename_toggle.grid(row=roww+3, column=0, sticky="E", padx=10, pady=(10, 5))
+
+        # Name given to files
+        nick_label = tk.Label(self, text="Name to be applied to files:", **label_style).grid(row=roww+3, column=1, **label_grid)
+        self.nick_label_entry = tk.Entry(self, textvariable=self.data_nickname, width=15)
+        self.nick_label_entry.grid(row=roww+3, column=1, sticky="E", padx=(0, 20), pady=(10, 5))
+
         # Starting number for anonymized files
-        start_number_label = tk.Label(self, text="Starting number:", **label_style).grid(row=roww+3, column=0, **label_grid)
-        start_number_entry = tk.Entry(self, textvariable=self.starting_number, width=10)
-        start_number_entry.grid(row=roww+3, column=1, sticky="W", padx=(0, 10))
+        start_number_label = tk.Label(self, text="Starting number:", **label_style).grid(row=roww+3, column=2, sticky='W', padx=(10, 5), pady=(10, 5))
+        self.start_number_entry = tk.Entry(self, textvariable=self.starting_number, width=10)
+        self.start_number_entry.grid(row=roww+3, column=2, sticky="E", padx=(10, 110), pady=(10, 5))
 
         input_label = tk.Label(self, text="Select Folder:", **label_style).grid(row=roww+4, column=0, **label_grid)
         input_entry = tk.Entry(self, textvariable=self.input_path, width=50)
         input_entry.grid(row=roww+4, column=1, sticky="EW", padx=(0, 10))
 
-        input_button = tk.Button(self, text="Browse", command=self.browse_input_path,font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
+        input_button = tk.Button(self, text="Browse", command=self.browse_input_path, font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
         input_button.grid(row=roww+4, column=2, sticky="W", padx=(10, 10))
 
-        open_button = tk.Button(self, text="Open Folder", command=self.open_folder,font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
+        open_button = tk.Button(self, text="Open Folder", command=self.open_folder, font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
         open_button.grid(row=roww+4, column=2, sticky="E", padx=(0, 80))
 
         convert_button = tk.Button(self, text="Start", command=self.convert_files, bg='#2196F3', fg='white',
@@ -210,7 +270,10 @@ class AnonDtoNGUI(tk.Frame):
                                    padx=20, pady=10)  # Increase padding
         convert_button.grid(row=roww+5, column=0, columnspan=3, padx=(100, 0), pady=(10, 20))
 
-        
+        # Initially disable the name and starting number fields
+        self.toggle_rename_fields()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = AnonDtoNGUI(root, root.destroy)  # For standalone testing, 'home' button will close the app
