b6d6ac2ff9b02ced7c56689bada033e6b8402ecd
alejandro-matos <84166343+alejandro-matos@users.noreply.github.com>
2024-06-12 23:10:09 -0600

    Create DCM2STL.py
    WIP of code to convert DICOM to STL directly

---
+++ nnUNetv2GUI/DCM2STL.py
@@ -0,0 +1,338 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog
+import dicom2nifti
+import os
+from pathlib import Path
+import sys
+import subprocess
+import threading
+import webbrowser
+import styles
+import pydicom
+import shutil
+from stl import mesh
+from PIL import Image, ImageTk
+import numpy as np
+import nibabel as nib
+
+class CustomCheckButton(tk.Label):
+    def __init__(self, parent, variable, on_image, off_image, command=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.variable = variable
+        self.on_image = on_image
+        self.off_image = off_image
+        self.command = command
+        self.configure(image=self.off_image, bg=styles.BG_COLOR)
+        self.bind("<Button-1>", self.toggle)
+
+    def toggle(self, event=None):
+        if self.variable.get():
+            self.variable.set(False)
+            self.configure(image=self.off_image)
+        else:
+            self.variable.set(True)
+            self.configure(image=self.on_image)
+        
+        if self.command:
+            self.command()
+
+class AirwaySegmenterGUI(tk.Frame):
+    def __init__(self, parent, home_callback):
+        super().__init__(parent)
+        self.parent = parent
+        self.home_callback = home_callback
+        self.configure(bg=styles.BG_COLOR)
+
+        self.input_path = tk.StringVar()
+        self.output_path = tk.StringVar()  # Added output path variable
+        self.starting_number = tk.IntVar(value=1)  # Added starting number variable
+        self.rename_files = tk.BooleanVar()  # Added rename files variable
+        self.data_nickname = tk.StringVar(value='Airways')
+
+        self.load_icons()
+        self.create_widgets()
+
+    def resource_path(self, relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        abs_path = Path(base_path) / relative_path
+        return abs_path.as_posix()
+
+    def browse_input_path(self):
+        selected_path = filedialog.askdirectory()
+        if selected_path:
+            self.input_path.set(selected_path)
+            print(f"Input path set to: {selected_path}")
+
+    def browse_output_path(self):
+        selected_path = filedialog.askdirectory()
+        if selected_path:
+            self.output_path.set(selected_path)
+            print(f"Output path set to: {selected_path}")
+
+    def load_icons(self):
+        try:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            checked_icon_path = os.path.join(base_dir, "Images", "checked.png")
+            unchecked_icon_path = os.path.join(base_dir, "Images", "unchecked.png")
+
+            image_size = 30
+
+            checked_icon = Image.open(checked_icon_path).resize((image_size, image_size), Image.ANTIALIAS)
+            self.checked_icon = ImageTk.PhotoImage(checked_icon)
+
+            unchecked_icon = Image.open(unchecked_icon_path).resize((image_size, image_size), Image.ANTIALIAS)
+            self.unchecked_icon = ImageTk.PhotoImage(unchecked_icon)
+
+        except Exception as e:
+            print(f"Error loading images: {e}")
+            self.checked_icon = None
+            self.unchecked_icon = None
+
+    def open_folder(self):
+        input_path_str = self.input_path.get()
+        parent_directory = os.path.dirname(input_path_str)
+        processed_dir = os.path.join(parent_directory, f"{os.path.basename(input_path_str)}_Processed_Images")
+        if processed_dir:
+            if not os.path.isdir(processed_dir):
+                messagebox.showwarning("Directory Error", f"The directory {processed_dir} does not exist. Please ensure that the correct folder is selected and/or that it has been processed.")
+            else:
+                print(f"Opening folder: {processed_dir}")
+                if sys.platform == "win32":
+                    os.startfile(processed_dir)
+                elif sys.platform == "darwin":
+                    os.system(f"open {processed_dir}")
+                else:
+                    os.system(f"xdg-open {processed_dir}")
+
+    def convert_files(self):
+        input_path_str = self.input_path.get()
+        rename_files = self.rename_files.get()
+        data_nick = self.data_nickname.get()
+
+        if not input_path_str:
+            messagebox.showwarning("Input Error", "Please select the input directory.")
+            return
+
+        # Get the parent directory of the input folder
+        parent_directory = os.path.dirname(input_path_str)
+        output_dir = os.path.join(parent_directory, f"{os.path.basename(input_path_str)}_Processed_Images")
+        self.output_path.set(output_dir)  # Set the output directory
+
+        central_nifti_folder = os.path.join(output_dir, "NIfTI")
+        os.makedirs(central_nifti_folder, exist_ok=True)
+
+        # File to store the mapping of original and anonymized folder names
+        if rename_files:
+            mapping_file_path = os.path.join(output_dir, "folder_mapping.txt")
+            with open(mapping_file_path, "w") as mapping_file:
+                mapping_file.write("Original Folder\tAnonymized Folder\n")
+
+        patient_folders = [f for f in os.listdir(input_path_str) if os.path.isdir(os.path.join(input_path_str, f))]
+        start_number = self.starting_number.get()
+        print(f"This is the output of patient_folders: {patient_folders}")
+        for patient_index, patient_folder in enumerate(patient_folders, start=start_number):
+            print(f"Processing patient folder: {patient_folder} with index {patient_index}")
+            patient_folder_path = os.path.join(input_path_str, patient_folder)
+
+            if rename_files:
+                anonymized_folder_name = f"{data_nick}_{patient_index}"  # Custom naming
+                # Log folder names to the mapping file
+                with open(mapping_file_path, "a") as mapping_file:
+                    mapping_file.write(f"{patient_folder}\t{anonymized_folder_name}\n")
+                patient_name = anonymized_folder_name
+            else:
+                anonymized_folder_name = patient_folder
+                patient_name = patient_folder
+
+            anonymized_folder_path = patient_folder_path
+
+            try:
+                dicom2nifti.convert_directory(anonymized_folder_path, central_nifti_folder, compression=True)
+            except dicom2nifti.exceptions.ConversionError as e:
+                print(f"Error converting {anonymized_folder_path}: {e}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+
+            nifti_files = [f for f in os.listdir(central_nifti_folder) if f.endswith('.nii') or f.endswith('.nii.gz')]
+            for nifti_file in nifti_files:
+                base_name, ext = os.path.splitext(nifti_file)
+                suffix = "_0000"
+
+                if ext == ".gz":
+                    base_name, ext2 = os.path.splitext(base_name)
+                    if rename_files:
+                        new_name = f"{data_nick}_{patient_index}{suffix}{ext2}{ext}"
+                    else:
+                        new_name = f"{patient_folder}{suffix}{ext2}{ext}"
+                else:
+                    if rename_files:
+                        new_name = f"{data_nick}_{patient_index}{suffix}{ext}"
+                    else:
+                        new_name = f"{patient_folder}{suffix}{ext}"
+
+                new_path = os.path.join(central_nifti_folder, new_name)
+                if not os.path.exists(new_path):
+                    print(f"Renaming NIfTI file {nifti_file} to {new_name}")
+                    os.rename(os.path.join(central_nifti_folder, nifti_file), new_path)
+
+        self.segment_airway(central_nifti_folder, output_dir)
+        self.convert_to_stl(output_dir)
+
+        messagebox.showinfo("Process Complete", "The selected operations have been completed.")
+
+    def segment_airway(self, nifti_folder, output_dir):
+        print(f"Segmenting airway using nnUNet on data in {nifti_folder}")
+
+        task_no = "Task001"
+        nnUNet_IN = nifti_folder
+        nnUNet_OUT = os.path.join(output_dir, "Segmentation")
+
+        def script_execution():
+            try:
+                Path1 = self.resource_path("Airways/nnUNet_raw")
+                Path2 = self.resource_path("Airways/nnUNet_results")
+                Path3 = self.resource_path("Airways/nnUNet_preprocessed")
+
+                os.environ["nnUNet_raw_data_base"] = Path1
+                os.environ["RESULTS_FOLDER"] = Path2
+                os.environ["nnUNet_preprocessed"] = Path3
+
+                result = subprocess.run([
+                    'nnUNetv2_predict', '-i', nnUNet_IN, '-o', nnUNet_OUT,
+                    '-d', '13', '-c', '3d_fullres',
+                ], capture_output=True, text=True)
+
+                if result.returncode != 0:
+                    raise subprocess.CalledProcessError(result.returncode, result.args, output=result.stdout, stderr=result.stderr)
+
+                messagebox.showinfo("Notification", f"The segmentation has been completed!")
+            except subprocess.CalledProcessError as e:
+                messagebox.showerror("Error", f"Failed to run the nnUNet prediction: {e.stderr}")
+
+        threading.Thread(target=script_execution).start()
+
+    def nifti_to_stl(self, nifti_file, stl_file):
+        # Load the NIfTI file
+        img = nib.load(nifti_file)
+        img_data = img.get_fdata()
+
+        # Ensure the data is binary
+        img_data = img_data.astype(np.uint8)
+        img_data[img_data > 0] = 1
+
+        # Perform marching cubes to extract the surface mesh
+        verts, faces, _, _ = measure.marching_cubes(img_data, level=0)
+
+        # Create a new mesh object
+        surface_mesh = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+
+        for i, f in enumerate(faces):
+            for j in range(3):
+                surface_mesh.vectors[i][j] = verts[f[j], :]
+
+        # Save the mesh as an STL file
+        surface_mesh.save(stl_file)
+
+    def convert_files(self):
+        input_path_str = self.input_path.get()
+        output_path_str = self.output_path.get()
+        
+        if not input_path_str or not output_path_str:
+            messagebox.showwarning("Input Error", "Please select both input and output directories.")
+            return
+
+        nifti_files = [f for f in os.listdir(input_path_str) if f.endswith('.nii') or f.endswith('.nii.gz')]
+        if not nifti_files:
+            messagebox.showwarning("Input Error", "No NIfTI files found in the selected input directory.")
+            return
+
+        for nifti_file in nifti_files:
+            nifti_file_path = os.path.join(input_path_str, nifti_file)
+            stl_file_path = os.path.join(output_path_str, f"{os.path.splitext(nifti_file)[0]}.stl")
+            self.nifti_to_stl(nifti_file_path, stl_file_path)
+
+        messagebox.showinfo("Conversion Complete", "All NIfTI files have been converted to STL files.")
+
+    def toggle_rename_fields(self):
+        if self.rename_files.get():
+            self.nick_label_entry.config(state=tk.NORMAL)
+            self.start_number_entry.config(state=tk.NORMAL)
+        else:
+            self.nick_label_entry.config(state=tk.DISABLED)
+            self.start_number_entry.config(state=tk.DISABLED)
+
+    def create_widgets(self):
+        home_button = tk.Button(self, text="Home", command=self.home_callback, bg='#FF9800', fg='white',
+                                font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE), padx=styles.BUTTON_PADDING, pady=styles.BUTTON_PADDING)
+        home_button.grid(row=0, column=0, padx=(10, 20), pady=(10, 20), sticky="W")
+
+        text_widget = tk.Text(self, wrap='word', height=18, width=86, bg=styles.BG_COLOR, fg='white', font=(styles.FONT_FAMILY, 13))
+        text_widget.insert(tk.END, """File Converter Module\n
+        This module processes DICOM images by converting them to NIfTI format,
+        and renaming the files to meet the algorithm's naming convention for proper identification.
+                   
+        Instructions:
+        - Use the toggle buttons to select tasks:
+                - Rename files: Numbers anonymized files starting 
+                    from the specified number in the "Starting number" line. A .txt file containing the 
+                    naming key relating the original and anonymized files is also created in this step.
+        - Click 'Browse' to select the folder containing the files.
+        - Input the starting number for numbering the anonymized files.
+        - Make sure that the folder contains only the DICOM files to be converted.
+        - Click 'Start' to begin processing the selected tasks.
+        - Once done, click 'Open Folder' to access the processed images and renaming key in the newly 
+          created folder called "{Folder Name}_Processed_Images".""")
+        text_widget.grid(row=1, column=0, columnspan=3, padx=20, pady=(10, 20))
+        text_widget.tag_configure("bold", font=(styles.FONT_FAMILY, 18, 'bold'))
+        text_widget.tag_configure("center", justify='center')
+        text_widget.tag_add("bold", "1.0", "1.end")
+        text_widget.tag_add("center", "1.0", "1.end")
+        text_widget.configure(state='disabled')
+
+        label_style_title = {'bg': styles.BG_COLOR, 'fg': 'white', 'font': (styles.FONT_FAMILY, styles.FONT_SIZE+4, 'bold')}
+        label_style = {'bg': styles.BG_COLOR, 'fg': 'white', 'font': (styles.FONT_FAMILY, styles.FONT_SIZE, 'bold')}
+        label_grid = {'sticky': tk.W, 'padx': 10, 'pady': (15, 10)}
+
+        roww = 2
+
+        # Add option asking whether to rename files or not
+        rename_label = tk.Label(self, text="Rename files?", **label_style)
+        rename_label.grid(row=roww+3, column=0, **label_grid)
+        rename_toggle = CustomCheckButton(self, variable=self.rename_files, on_image=self.checked_icon, off_image=self.unchecked_icon, command=self.toggle_rename_fields)
+        rename_toggle.grid(row=roww+3, column=0, sticky="E", padx=(0,120), pady=(10, 5))
+
+        # Name given to files
+        nick_label = tk.Label(self, text="Name to be applied to files:", **label_style).grid(row=roww+3, columnspan=2,column=0, sticky="W", padx=(200, 0), pady=(10,5))
+        self.nick_label_entry = tk.Entry(self, textvariable=self.data_nickname, width=9,font=styles.FONT_SIZE-3)
+        self.nick_label_entry.grid(row=roww+3, column=1, sticky="E", padx=(0, 0), pady=(10, 5))
+
+        # Starting number for anonymized files
+        start_number_label = tk.Label(self, text="Starting number:", **label_style).grid(row=roww+3, column=2, sticky='W', padx=(10, 5), pady=(10, 5))
+        self.start_number_entry = tk.Entry(self, textvariable=self.starting_number, width=5, font=styles.FONT_SIZE-3)
+        self.start_number_entry.grid(row=roww+3, column=2, sticky="E", padx=(10, 80), pady=(10, 5))
+
+        input_label = tk.Label(self, text="Select Folder:", **label_style).grid(row=roww+4, column=0, **label_grid)
+        input_entry = tk.Entry(self, textvariable=self.input_path, width=65)
+        input_entry.grid(row=roww+4, columnspan=2,column=0, sticky="W", padx=(130, 0))
+
+        input_button = tk.Button(self, text="Browse", command=self.browse_input_path, font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
+        input_button.grid(row=roww+4, column=2, sticky="W", padx=(10, 15))
+
+        open_button = tk.Button(self, text="Open Results Folder", command=self.open_folder, font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
+        open_button.grid(row=roww+4, column=2, sticky="E", padx=(0, 10))
+
+        convert_button = tk.Button(self, text="Start", command=self.convert_files, bg='#2196F3', fg='white',
+                                   font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE),  # Increase font size
+                                   padx=20, pady=10)  # Increase padding
+        convert_button.grid(row=roww+6, column=0, columnspan=3, padx=(100, 0), pady=(10, 20))
+
+        # Initially disable the name and starting number fields
+        self.toggle_rename_fields()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AirwaySegmenterGUI(root, root.destroy)  # For standalone testing, 'home' button will close the app
+    app.pack(fill=tk.BOTH, expand=True)
+    root.mainloop()
