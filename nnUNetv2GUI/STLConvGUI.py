1ef3c991604e2331eeefa133da010f8fb91a7a9d
alejandro-matos <84166343+alejandro-matos@users.noreply.github.com>
2024-08-01 13:23:44 -0600

    Update STLConvGUI.py
    Changed from tkinter to customtkinter

--- nnUNetv2GUI/STLConvGUI.py
+++ nnUNetv2GUI/STLConvGUI.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import filedialog, messagebox
 import os
 from pathlib import Path
@@ -6,23 +6,24 @@ import sys
 import styles
 import vtk
 
-class STLConverterGUI(tk.Frame):
+class STLConverterGUI(ctk.CTkFrame):
     def __init__(self, parent, home_callback):
         super().__init__(parent)
         self.parent = parent
         self.home_callback = home_callback
-        self.configure(bg=styles.BG_COLOR)
+        
+        # Set the size of the frame to match the main window
+        self.configure(width=styles.WINDOW_WIDTH, height=styles.WINDOW_HEIGHT)
 
-        self.input_path = tk.StringVar()
-        self.output_path = tk.StringVar()
+        self.input_path = ctk.StringVar()
+        self.output_path = ctk.StringVar()
 
         self.create_widgets()
 
     def resource_path(self, relative_path):
-        """ Get absolute path to resource, works for dev and for PyInstaller """
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
         abs_path = Path(base_path) / relative_path
-        return abs_path.as_posix()  # Convert to POSIX (i.e., forward-slash) format
+        return abs_path.as_posix()
 
     def browse_input_path(self):
         self.input_path.set(filedialog.askdirectory())
@@ -136,8 +137,8 @@ class STLConverterGUI(tk.Frame):
             nifti_file_path = os.path.join(input_path_str, nifti_file)
             base_name = os.path.splitext(os.path.splitext(nifti_file)[0])[0]
             stl_file_path = os.path.join(output_path_str, f"{base_name}.stl")
-            print (f"The base name is {base_name}")
-            print (f"The stl_file_path is {stl_file_path}")
+            print(f"The base name is {base_name}")
+            print(f"The stl_file_path is {stl_file_path}")
             self.nifti_to_stl(nifti_file_path, stl_file_path, threshold_value=1, decimate=True, decimate_target_reduction=0.5)
 
         messagebox.showinfo("Conversion Complete", "All NIfTI files have been converted to STL files.")
@@ -145,45 +146,44 @@ class STLConverterGUI(tk.Frame):
     def create_widgets(self):
         # Home button
         if self.home_callback:
-            home_button = tk.Button(self, text="Home", command=self.home_callback, bg='#FF9800', fg='white',
-                                    font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE), padx=styles.BUTTON_PADDING, pady=styles.BUTTON_PADDING)
-            home_button.grid(row=0, column=0, padx=(10, 20), pady=(10, 20), sticky="W")
+            home_button = ctk.CTkButton(self, text="Home", command=self.home_callback, fg_color='#FF9800', text_color='white',
+                                        font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE))
+            home_button.grid(row=0, column=0, padx=(10, 20), pady=(10, 20), sticky="w")
 
         # Text widget
-        text_widget = tk.Text(self, wrap='word', height=9, width=86, bg=styles.BG_COLOR, fg='white', font=(styles.FONT_FAMILY, 13))
-        text_widget.insert(tk.END, """\nNIfTI to STL Converter\n\n
+        text_widget = ctk.CTkTextbox(self, wrap='word', height=200, width=600)
+        text_widget.insert("1.0", """\nNIfTI to STL Converter\n\n
         - Click on 'Browse' to select the folder containing the segmentation files in NIfTI format.
         - Browse to the folder where STL files will be saved.
         - Click on 'Convert' to initialize the conversion.""")
         text_widget.grid(row=1, column=0, columnspan=3, padx=20, pady=(10, 20))
-        text_widget.tag_configure("bold", font=(styles.FONT_FAMILY, 18, 'bold'))
-        text_widget.tag_configure("center", justify='center')
-        text_widget.tag_add("bold", "2.0", "3.end")
-        text_widget.tag_add("center", "2.0", "3.end")
         text_widget.configure(state='disabled')  # Make the text widget read-only
 
-        label_grid = {'sticky': tk.E, 'padx': 10, 'pady': (10, 10)}
-        roww = 1
-
         # Input folder
-        tk.Label(self, text="Segmentations Folder:", bg=styles.BG_COLOR, fg='white', font=(styles.FONT_FAMILY, styles.FONT_SIZE, 'bold')).grid(row=roww+1, columnspan=1, **label_grid)
-        tk.Entry(self, textvariable=self.input_path, width=70).grid(row=roww+1, column=1, sticky="W")
-        tk.Button(self, text="Browse", command=self.browse_input_path, font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=roww+1, column=2, sticky="W", padx=(10, 20))
+        ctk.CTkLabel(self, text="Segmentations Folder:", font=(styles.FONT_FAMILY, styles.FONT_SIZE, 'bold')).grid(row=2, column=0, sticky="e", padx=10, pady=(10, 10))
+        ctk.CTkEntry(self, textvariable=self.input_path, width=400).grid(row=2, column=1, sticky="w")
+        ctk.CTkButton(self, text="Browse", command=self.browse_input_path, font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=2, column=2, sticky="w", padx=(10, 20))
 
         # Output folder
-        tk.Label(self, text="STL Folder:", bg=styles.BG_COLOR, fg='white', font=(styles.FONT_FAMILY, styles.FONT_SIZE, 'bold')).grid(row=roww+2, columnspan=1, **label_grid)
-        tk.Entry(self, textvariable=self.output_path, width=70).grid(row=roww+2, column=1, sticky="W")
-        tk.Button(self, text="Browse", command=self.browse_output_path, font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=roww+2, column=2, sticky="W", padx=(10, 20))
+        ctk.CTkLabel(self, text="STL Folder:", font=(styles.FONT_FAMILY, styles.FONT_SIZE, 'bold')).grid(row=3, column=0, sticky="e", padx=10, pady=(10, 10))
+        ctk.CTkEntry(self, textvariable=self.output_path, width=400).grid(row=3, column=1, sticky="w")
+        ctk.CTkButton(self, text="Browse", command=self.browse_output_path, font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=3, column=2, sticky="w", padx=(10, 20))
         
         # Convert button
-        tk.Button(self, text="Start", command=self.convert_files, bg='#2196F3', fg='black', font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=roww+3, column=1, sticky="EW", padx=(50, 50),pady=(10, 20))
+        ctk.CTkButton(self, text="Start", command=self.convert_files, fg_color='#2196F3', text_color='black', font=(styles.FONT_FAMILY, styles.FONT_SIZE)).grid(row=4, column=1, sticky="ew", padx=(50, 50), pady=(10, 20))
 
         # Open button
-        open_button = tk.Button(self, text="Open Results Folder", command=self.open_folder,font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2), padx=10, pady=0)
-        open_button.grid(row=roww+3, column=2, sticky="E", padx=(0, 80), pady=(0,10))
+        open_button = ctk.CTkButton(self, text="Open Results Folder", command=self.open_folder, font=(styles.FONT_FAMILY, styles.BUTTON_FONT_SIZE-2))
+        open_button.grid(row=4, column=2, sticky="e", padx=(0, 80), pady=(0, 10))
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    ctk.set_appearance_mode("dark")  # Set the appearance mode to dark
+    ctk.set_default_color_theme("blue")  # Set the color theme to blue
+
+    root = ctk.CTk()
+    root.title("NIfTI to STL Converter")
+    root.geometry(f"{styles.WINDOW_WIDTH}x{styles.WINDOW_HEIGHT}")  # Set fixed geometry
+    root.resizable(False, False)  # Disable window resizing
     app = STLConverterGUI(root, root.destroy)  # For standalone testing, 'home' button will close the app
-    app.pack(fill=tk.BOTH, expand=True)
+    app.pack(fill="both", expand=True)
     root.mainloop()
