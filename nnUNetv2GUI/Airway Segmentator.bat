d0165d6024fde2de2f574601b1312a4b42cb917a
Alejandro Matos <amatos@ualberta.ca>
2024-11-20 21:53:59 -0700

    changing bat file to address main.py instead of main_v_0p3

--- nnUNetv2GUI/Airway Segmentator.bat
+++ nnUNetv2GUI/Airway Segmentator.bat
@@ -1,4 +1,4 @@
 @echo off 
 call C:\Users\karlacarpiohorta\Desktop\Silvia\Odonto\venv\nnunetv2env\Scripts\activate
-python C:\Users\karlacarpiohorta\Desktop\Silvia\Odonto\AirwaySegmentatorGUI-1\nnUNetv2GUI\main_v_0p3.py
+python C:\Users\karlacarpiohorta\Desktop\Silvia\Odonto\AirwaySegmentatorGUI-1\nnUNetv2GUI\main.py
 
